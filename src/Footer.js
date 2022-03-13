76bb7350c6d89d35855842acb38a6c9a34f93ab0
maikolcw <whysoraccoony@gmail.com>
2022-03-13 15:34:02 -0700

    Fix links not working on azure

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './App.css'
+import { Link } from 'react-router-dom';
 
 const Footer = () => (
   <>
 <footer class="border-top footer text-muted">
         <div b-617jbvk8u3 class="container">
-            &copy; 2022 - FizzBuzzWeb - <a href="/privacy">Privacy</a>
+            &copy; 2022 - FizzBuzzWeb - <Link to="/privacy">Privacy</Link>
         </div>
     </footer>
   </>  
--- src/NavBar.js
+++ src/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => (
   <title>Home Page - FizzBuzzWeb</title>
     <nav b-617jbvk8u3 class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
         <div b-617jbvk8u3 class="container-fluid">
-            <a class="navbar-brand" href="/">FizzBuzzWeb</a>
+        <Link class="navbar-brand" to="/">FizzBuzzWeb</Link>
             <button b-617jbvk8u3 class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                 <span b-617jbvk8u3 class="navbar-toggler-icon"></span>
@@ -13,10 +13,10 @@ const NavBar = () => (
             <div b-617jbvk8u3 class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
                 <ul b-617jbvk8u3 class="navbar-nav flex-grow-1">
                     <li b-617jbvk8u3 class="nav-item">
-                        <a class="nav-link text-dark" href="/">Home</a>
+                    <Link class="nav-link text-dark" to="/">Home</Link>
                     </li>
                     <li b-617jbvk8u3 class="nav-item">
-                        <a class="nav-link text-dark" href="/privacy">Privacy</a>
+                    <Link class="nav-link text-dark" to="/privacy">Privacy</Link>
                     </li>
                 </ul>
             </div>
